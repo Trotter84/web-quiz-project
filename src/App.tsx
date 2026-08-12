fce20e9247bc093ebaab7d9a870b22692935923d
davidsonaparker <davidsonaparker@gmail.com>
2026-08-12 09:54:50 -0600

    Fixed input boxes to allow typing.

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ export default function App() {
         <Routes>
             {/*<Route path={'/'} element={<Login/>}/>*/}
             <Route path={'/home'} element={<Home/>}/>
-            <Route path={'/SignUp'} element={<SignUp/>}/>
+            <Route path={'/sign-up'} element={<SignUp/>}/>
         </Routes>
     </>
   )
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -19,19 +19,18 @@ function SignUp()
         setStoredPassword(passwordValue);
         setUsernameValue(""); // This should clear the text box
     }
-
+    if (storedUsername === "" && storedPassword === ""){}
     // This is going to be a sign-up header with a username and password input boxes and then Sign up button to store them.
     return (
         <>
-            <h3>Sign Up</h3>
-            <input type="text" value={storedUsername} onChange={(input) => setUsernameValue(input.target.value)}>Username</input>
-            <input type="text" value={storedPassword} onChange={(input) => setPasswordValue(input.target.value)}>Password</input>
+            <h1>Sign Up</h1>
+            <input type="text" value={usernameValue} onChange={(input) => setUsernameValue(input.target.value)}/>
+            <input type="text" value={passwordValue} onChange={(input) => setPasswordValue(input.target.value)}/>
             <button onClick={() => {
                 handleStoredUsername();
                 handleStoredPassword();
             }
             }>Sign Up</button>
-        </>
-    );
+        </>);
 }
 export default SignUp;

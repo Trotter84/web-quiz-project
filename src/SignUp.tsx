e4b5abef30d18f1bdb147a4c7168f42bd6cd9cdd
davidsonaparker <davidsonaparker@gmail.com>
2026-08-12 09:09:55 -0600

    Create SignUp.tsx
    Created the Sign up file and add some basic elements to get started.

---
+++ src/SignUp.tsx
@@ -0,0 +1,16 @@
+//import {useState} from "react";
+
+function SignUp()
+{
+    // const [state, setState] = useState("");
+
+    // This is going to be a sign-up header with a username and password input boxes
+    return (
+        <>
+            <h3>Sign Up</h3>
+            <input type="text">Username</input>
+            <input type="text">Password</input>
+        </>
+    );
+}
+export default SignUp;

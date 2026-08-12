2525beb69fc36a5093c8725ba3328c4f3224d07c
davidsonaparker <davidsonaparker@gmail.com>
2026-08-12 09:35:22 -0600

    Update App.tsx
    Added Sign Up to the App react file

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import './styles/App.css'
 
 import Home from './pages/home.tsx'
+import SignUp from "./pages/SignUp.tsx";
 
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Routes>
             {/*<Route path={'/'} element={<Login/>}/>*/}
             <Route path={'/home'} element={<Home/>}/>
+            <Route path={'/SignUp'} element={<SignUp/>}/>
         </Routes>
     </>
   )

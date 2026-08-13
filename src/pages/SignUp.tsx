fe33e4980ec6de4deea777ad1b25c01ddef0b996
davidsonaparker <davidsonaparker@gmail.com>
2026-08-13 08:51:34 -0600

    Changed spacing

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -28,7 +28,7 @@ function SignUp()
         setPasswordValue(""); // This should clear the password text box
     }
 
-    // Stores user data to a JSON file which will later be stored into mongdb in a different function
+    // Stores user data to a JSON file which will later be stored into mongodb in a different function
     // function saveDataJson()
     // {
     //     const userName = storedUsername;
@@ -40,6 +40,7 @@ function SignUp()
     // I need to have the user schema to know how to store the username and password in a proper JSON format
 
     if (storedUsername === "" && storedPassword === ""){} //Useless if statement to use the stored variables to not make the compiler mad for now. These will be used to store the users info into a JSON format
+
     // This is going to be a sign-up header with a username and password input boxes and then Sign up button to store them.
     return ( // I need to center the elements and make the width of the input boxes smaller
         <>

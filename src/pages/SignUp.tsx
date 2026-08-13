f36f0d87f70bcf1022ed51392ebc790a51214858
davidsonaparker <davidsonaparker@gmail.com>
2026-08-13 09:07:28 -0600

    Changed spacing

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,11 +2,6 @@ import {useState} from "react";
 //import * as fs from 'fs';
 
 
-//Save user data to a json file and send it to the mongodb
-
-
-
-
 // I will likely use this code structure to make the login page as well. I might eventually make a more general method to handle input boxes overall for login, sign up, answer quiz questions etc.
 function SignUp()
 {
@@ -29,6 +24,7 @@ function SignUp()
     }
 
     // Stores user data to a JSON file which will later be stored into mongodb in a different function
+
     // function saveDataJson()
     // {
     //     const userName = storedUsername;
@@ -42,7 +38,8 @@ function SignUp()
     if (storedUsername === "" && storedPassword === ""){} //Useless if statement to use the stored variables to not make the compiler mad for now. These will be used to store the users info into a JSON format
 
     // This is going to be a sign-up header with a username and password input boxes and then Sign up button to store them.
-    return ( // I need to center the elements and make the width of the input boxes smaller
+    // I need to center the elements and make the width of the input boxes smaller
+    return (
         <>
             <h1>Sign Up</h1>
             <input type="text" value={usernameValue} onChange={(input) => setUsernameValue(input.target.value)}/>

8ba26672b887a9f03b753f5285c78cc03c57afcd
davidsonaparker <davidsonaparker@gmail.com>
2026-08-18 09:20:39 -0600

    Added a basic mongodb connection

--- index.ts
+++ index.ts
@@ -1,7 +1,13 @@
 import express from 'express'
 import cors from "cors";
+import mongoose from 'mongoose';
 const app = express();
 const PORT = 3000;
+
+mongoose.connect('mongodb+srv://gcrichton_db_user:Didq48hWtx5N46gV@cluster0.irlkoas.mongodb.net/')
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error(err));
+
 
 app.use(cors());
 

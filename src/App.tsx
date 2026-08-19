a5e47d8b72271e54b0bd258938079185e698fbac
Daniel Trotter <flare84@gmail.com>
2026-08-19 09:35:56 -0600

    adjusted routes

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,12 @@ export default function App() {
     return (
         <>
             <Routes>
-                {/*<Route path={'/'} element={<Login/>}/>*/}
-                <Route path={'/home'} element={<Home/>}/>
+                <Route path={'/'} element={<Home/>}/>
                 <Route path={'/sign-up'} element={<SignUp/>}/>
                 <Route path={'/login'} element={<Login/>}/>
-
+                {/*<Route path={'/home'} element={<Home/>}/>*/}
+                {/*<Route path='*' element={<PageNotFound/>}/>*/}
+                <Route path='*' element={<Home/>}/>
             </Routes>
         </>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import '../styles/home.css'
 import QuizContainer from "./QuizScreen.tsx";
+import CategoryCard from "../components/cards/CategoryCard.tsx";
 
 export default function Home() {
     return (
         <>
             <h1>Home Page</h1>
+            <CategoryCard/>
             <QuizContainer/>
         </>
     );

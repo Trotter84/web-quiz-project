a1aaddba06fdef25934774d2f4b3369cc1fb58f7
Daniel Trotter <flare84@gmail.com>
2026-08-31 16:31:45 -0600

    added 'space/enter'

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -7,7 +7,7 @@ export default function BottomBar() {
         <div className="bottomBar-container">
             <p>*typing is case <i>insensitive</i></p>
             <p>*use <b>tab</b> and <b>shift+tab</b> to cycle through multiple choice</p>
-            <p>*use <b>space</b> to select</p>
+            <p>*use <b>space</b>/<b>enter</b> to select</p>
         </div>
     )
 }
